e80cf81eb69a846492dbb1174f2d839783eeb2f3
Ｔｕｓｈａｒ　Ｔａｋ <tushartak861@gmail.com>
2023-08-19 23:14:44 +0530

    Added Image upload in Tweet API

--- src/controller/tweetController.js
+++ src/controller/tweetController.js
@@ -1,20 +1,27 @@
 import TweetService from '../services/tweetService.js';
-
+import upload from '../config/file-upload-s3-config.js';
 
 const tweetService = new TweetService();
 
-
+const singleUploader = upload.single('image');
 
 const createTweet = async (req, res) => {
     try {
         
-            const tweet = await tweetService.create(req.body);
+        singleUploader(req, res, async function (err, data) {
+            if(err) {
+                return res.status(500).json({error: err});
+            }
+            const payload = {...req.body};
+            payload.image = req.file.location;
+            const tweet = await tweetService.create(payload);
             return res.status(201).json({
                 data: tweet,
                 success: true,
                 message: 'Successfully created!!',
                 err: {}
             });
+        });
         
     } catch (error) {
         console.log(error);
